2097795ce87edeab1233be65def29d58788fd48d
Sergio lopez <141697078+sergioAlexanderLopEsc@users.noreply.github.com>
2023-09-20 18:37:21 -0600

    cambió

---
+++ prácticasX/rport.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REPORTE</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col"><br><br>
+            <div class="card tex-center">
+                  <div class="card-header">
+                      Licenciatura en sistemas computacionales <br> Administracion de base de datos
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">Ejercicio 1-2-3<br>Segundo parcial</h5>
+                      <p class="card-text">Reportes con pdf en php.</p>
+                      <a href="#" class="btn btn-dark">reporte 1</a><br><br>
+                    </div>
+              </div>
+        </div>
+    </div>
+</body>
+</html>

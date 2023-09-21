c50ad12a9ab5fe5fbcaa3880846dad7fb90cfb93
Sergio lopez <141697078+sergioAlexanderLopEsc@users.noreply.github.com>
2023-09-20 18:50:22 -0600

    cambió

--- prácticasX/rport.php
+++ prácticasX/rport.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>REPORTE</title>
-</head>
+    <!--Bootstrap CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>    
 <body>
     <div class="container">
         <div class="row">
@@ -18,9 +20,15 @@
                       <h5 class="card-title">Ejercicio 1-2-3<br>Segundo parcial</h5>
                       <p class="card-text">Reportes con pdf en php.</p>
                       <a href="#" class="btn btn-dark">reporte 1</a><br><br>
+                      <a href="#" class="btn btn-dark">reporte 1</a><br><br>
+                      <a href="#" class="btn btn-dark">reporte 1</a><br><br>
                     </div>
               </div>
         </div>
     </div>
+<!--Bootstrap JS 5.3-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> 
 </body>
 </html>
